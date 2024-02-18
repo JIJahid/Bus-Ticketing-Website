da86173ba14cdca5286d438d7cee5e307bc9a19b
JIJahid <j.i.jahid90@gmail.com>
2024-02-18 23:10:34 +0600

    success full added

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function next(){
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+
+    //show succes secreen
+    const successScreen = document.getElementById('success-screen');
+    successScreen.classList.remove('hidden')
+};
+
